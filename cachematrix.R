6c92290bd823be6b8513df1856f7bf15c0fb70ea
misspelled <Whohears@icloud.com>
2015-09-18 09:20:16 -0500

    Final clean up and testing
    Make function names mirror what was in Roger's stub. Do some more
    testing.

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,7 @@
 # the object stores the inverse so it can be retrieved without being 
 # recomputed. 
 
-initializeMatrix <- function(mtrx) {
+makeMatrix <- function(mtrx) {
     inv <- NULL
     list(get = function() {mtrx},      # return the matrix currently assigned to object
          set = function(new_mtrx) {    # function to assign different matrix to object
@@ -17,12 +17,12 @@ initializeMatrix <- function(mtrx) {
     )
 }
 
-# This function uses the object returned by initializeMatrix() to test if 
+# This function uses the object returned by makeMatrix() to test if 
 # the inverse of a matrix has been computed. It retrieves the stored inverse
 # if available. If not, it computes the the inverse and then stores it via a call to 
-# $set_inv 
+# makeMatrix$set_inv() 
 
-getCachedInverse <- function(m,...) {   # m is return value from initializeMatrix()
+cacheinverse <- function(m,...) {   # m is return value from makeMatrix()
     tst <- m$get_inv()                  # test if inverse has been computed
     if(!is.null(tst)) {                 # and return it if it has
         return(tst)                     
@@ -32,18 +32,27 @@ getCachedInverse <- function(m,...) {   # m is return value from initializeMatri
     inv    
 }
 
-# construct a matrix and initialize first function for testing
+# construct a matrix and call makeMatrix()  for testing
 vec <- rpois(1000000, 50)
 test_matrix <- matrix(vec, nrow = 1000, ncol = 1000)
 rm(vec)
-cache_store <- initializeMatrix(test_matrix)
+cache_store <- makeMatrix(test_matrix)
 
 # compute system time to confirm inverse is being retrieved
 # and calculated appropriately
 
-inverse1 <- system.time(getCachedInverse(cache_store))
-inverse2 <- system.time(getCachedInverse(cache_store))
+inverse1 <- system.time(cacheinverse(cache_store))
+inverse2 <- system.time(cacheinverse(cache_store))
+print(inverse1) # system time for calculation
+print(inverse2) # system time for retrieval
+inverse1['user.self'] > inverse2['user.self'] # should return TRUE
 
-#cache_store$set_inv(test_matrix)
-#cache_store$get_inv()
-#system.time(solve(test_matrix))
+
+# test that inverse is reset to NULL when matrix
+# is changed in makeMatrix()
+
+vec <- runif(1000000)
+new_test_matrix <- matrix(vec, nrow = 1000, ncol = 1000)
+cache_store$set(new_test_matrix)
+is.null(cache_store$get_inv()) # should return TRUE
+
